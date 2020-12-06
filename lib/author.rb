0f3504332a3d4c4b4672162c1bbe654806f905f6
kimalibennett <aminbennett@gmail.com>
2020-12-05 19:34:53 -0600

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,33 @@
+class Author
+    attr_accessor :name
+    @@post_count = 0
+  
+    def initialize(name)
+      @name = name
+    end
+  
+    def add_post_by_title(title)
+      post = Post.new(title, self)
+    end
+  
+    def posts
+      Post.all.select { |post| post.author == self }
+    end
+
+    def add_post(post)
+        post.author = self
+    end
+    
+    def posts_by_title
+      authors_posts = []
+      post.all_by_title.each do |post|
+        if post.author == self 
+          authors_posts << post.title 
+        end  
+      end
+      def self.post_count
+        @@post_count
+      end
+    
+  end
+end
